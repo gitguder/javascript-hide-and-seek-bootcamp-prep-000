5d7cbd4c345b963abcaac600b571328f225ddb57
Codey Pinero <codey.pinero@gmail.com>
2019-02-24 00:07:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  var ranks = document.querySelectorAll('ul. ranked-list li');
+  var upranks = document.getElementById('ul. ranked-list li');
   for (var i = 0; i < ranks.length; i++) {
     ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
   }

9bba48ba2ff7b35714e6a5a55296cfbe34c902c2
Codey Pinero <codey.pinero@gmail.com>
2019-02-24 00:15:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,5 @@ function increaseRankBy(n) {
 
 function deepestChild() {
   var deep = document.getElementById('grand-node').querySelectorAll('div');
-  return deep
-
-  }
+  return deep[deep.length - 1]
+}

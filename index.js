fb4d0eb6fbd120054b525d50a5046b4d10ace542
Codey Pinero <codey.pinero@gmail.com>
2019-02-24 00:06:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return document.getElementById('nested').querySelector
+  return document.getElementById('nested').querySelector('div.target')
 }
 
 function increaseRankBy(n) {
